630a3ac4d0047af4d6ff898d434b412887ce8750
Chepkasov Ruslan <chepkasov.ruslan01@gmail.com>
2023-12-09 15:50:54 +0300

    add color style, add navigation menu

--- app/src/main/java/com/example/divesanimaandroid/MainActivity.kt
+++ app/src/main/java/com/example/divesanimaandroid/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.divesanimaandroid
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.bottomNav
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.authorizationFragment)
+                bottomNav.visibility = View.GONE
+            else bottomNav.visibility = View.VISIBLE
+        }
     }
+
 }
